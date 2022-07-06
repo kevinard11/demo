9321835e01e88be42fd3fa49586b868283ed0655
kevinard11 <drlobsterard@gmail.com>
2022-07-06 16:50:46 +0700

    Add swagger tag and description for demo controller

--- src/main/java/com/yukiii/demo/controller/demo/DemoController.java
+++ src/main/java/com/yukiii/demo/controller/demo/DemoController.java
@@ -1,5 +1,6 @@
 package com.yukiii.demo.controller.demo;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("demo")
+@Tag(name = "Demo", description = "Controller for demo")
 public class DemoController {
 
     @RequestMapping(method = RequestMethod.GET)
