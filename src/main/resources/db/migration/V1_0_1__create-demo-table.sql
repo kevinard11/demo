e1bb4caa3e56ee143e683f092623fd8074f5bd34
kevinard11 <drlobsterard@gmail.com>
2022-07-06 12:19:34 +0700

    Create script for create table for demo

---
+++ src/main/resources/db/migration/V1_0_1__create-demo-table.sql
@@ -0,0 +1,16 @@
+create table demo(
+
+    id          bigserial,
+    demo_id     varchar,
+    demo_name   varchar(100),
+    desription  text,
+    level       int,
+    active      boolean,
+    version     int,
+    created_at  timestamp with time zone default current_timestamp,
+    updated_at  timestamp with time zone,
+    updated_by  varchar(100),
+    deleted_at  timestamp with time zone,
+
+    constraint pk_demo primary key(id)
+);
