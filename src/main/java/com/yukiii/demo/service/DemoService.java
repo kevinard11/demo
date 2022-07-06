f3201b03fc576237b3d16d80d12d87ac0132de8f
kevinard11 <drlobsterard@gmail.com>
2022-07-06 20:47:44 +0700

    Create demo service

---
+++ src/main/java/com/yukiii/demo/service/DemoService.java
@@ -0,0 +1,6 @@
+package com.yukiii.demo.service;
+
+import com.yukiii.demo.dto.demo.DemoCreateDto;
+
+public interface DemoService {
+}
---
+++ src/main/java/com/yukiii/demo/service/impl/DemoServiceImpl.java
@@ -0,0 +1,12 @@
+package com.yukiii.demo.service.impl;
+
+import com.yukiii.demo.dto.demo.DemoCreateDto;
+import com.yukiii.demo.mapper.DemoMapper;
+import com.yukiii.demo.repository.DemoRepository;
+import com.yukiii.demo.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("demoServiceImpl")
+public class DemoServiceImpl implements DemoService {
+}
